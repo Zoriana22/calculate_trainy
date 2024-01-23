a4577aeef5348f37639f270c37b79da7daa458c0
PaninaAY <PaninaAY@bk.ru>
2024-01-23 22:10:22 +0200

    plus_btn ok

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const first_count = document.querySelector('first');
-const second_count = document.querySelector('second');
-const result = document.querySelector('result');
+let first_count = document.querySelector('.first');
+console.log(first_count);
+let second_count = document.querySelector('.second');
+console.log(first_count);
+let result = document.querySelector('.result');
 
-const plus_btn = document.querySelector('plus');
-const minus_btn = document.querySelector('minus');
-const multi_btn = document.querySelector('multiplication');
-const division_btn = document.querySelector('division');
 
-function sum() {
-    plus_btn.addEventListener('click', function () {
-        result.innerHTML = first_count.value + second_count.value;
-        console.log(result)
-        return result;
-    });
-}
+const plus_btn = document.querySelector('.plus');
+const minus_btn = document.querySelector('.minus');
+const multi_btn = document.querySelector('.multiplication');
+const division_btn = document.querySelector('.division');
+
+
+plus_btn.addEventListener('click', function () {
+    result.innerHTML = Number(first_count.value) + Number(second_count.value);
+    console.log(result);
+});
+
 function minus() {
     minus_btn.addEventListener('click', function () {
         result.innerHTML = first_count.value - second_count.value;
@@ -34,8 +36,5 @@ function division() {
         console.log(result)
         return result;
     });
-} 
-sum();
-minus();
-multi();
-division();
+}
+
